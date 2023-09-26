d3bbbe616ee28051033c5f95f0d5bfc790b3e87f
den <khausden@gmail.com>
2023-09-26 20:02:49 +1000

    add gh pages, routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from '@/views/MainPage.vue'
 import Favorites from '@/views/favorites/index.vue'
 import Peoples from '@/views/peoples/index.vue'
 import Person from '@/views/peoples/_id.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory (import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
